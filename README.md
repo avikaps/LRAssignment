51be810f9a65740c9ea8031fa85bbe25f81f8dc6
Avi Kapoor IN1019723 <avikapoor@vmware.com>
2019-12-30 23:11:05 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. Unzip the package.
+2. Activate the virtual environment (votr). <folder_path>\\LRAssigment\votr\Scripts\activate.bat 
+3. Execute command - flask run
+4. Open "http://127.0.0.1:5000/" URL and navigate.
---
+++ app/routes.py
@@ -0,0 +1,91 @@
+from flask import render_template, flash, redirect, url_for
+from flask_wtf import Form
+from app import app
+from app import forms
+from app.forms import LoginForm
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from app import db
+from app.forms import RegistrationForm
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+
+admin = Admin(app, name='Results')
+admin.add_view(ModelView(User, db.session))
+
+poll_data = {
+   'question' : 'Which Team will win IPL 2020 ? ',
+   'fields'   : ['Chennai Super Kings','Delhi Capitals', 'Kings XI Punjab', \
+				 'Kolkata Knight Riders', 'Mumbai Indians', 'Rajasthan Royals', \
+				 'Royal Challengers Bangalore','Sunrisers Hyderabad']
+}
+
+filename = 'data.txt'
+
+@app.route('/')
+@app.route('/index')
+@login_required
+
+def index():
+    user = {'username': 'Anonymous'}
+    return render_template('index.html', title='Home Page', data=poll_data)
+
+@app.route('/poll', methods=['GET', 'POST'])
+def poll():
+    vote = request.args.get('field')
+    out = open(filename, 'a')
+    out.write( vote + '\n' )
+    out.close()
+    return render_template('thankyou.html', data=poll_data)
+ 
+@app.route('/results', methods=['GET', 'POST'])
+def show_results():
+    votes = {}
+    for f in poll_data['fields']:
+        votes[f] = 0
+    f  = open(filename, 'r')
+    for line in f:
+        vote = line.rstrip("\n")
+        votes[vote] += 1
+ 
+    return render_template('results.html', data=poll_data, votes=votes)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
